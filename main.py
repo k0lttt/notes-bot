e55b9c9619121e4134df95906ac82c09d2263545
k0lttt <superpupkin07770@gmail.com>
2026-03-17 03:15:11 +0300

    commit 1.3 / redis

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import asyncio 
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
 
 from my_config import config_bot
 from app.handlers import router
 
+import redis.asyncio as aioredis
+
 async def main():
+    redis = await aioredis.from_url(f"redis://localhost:6379/0")
     logging.basicConfig(level = logging.INFO, filename = "py_logbot.log", filemode = "w")
     bot = Bot(token=config_bot.bot_token.get_secret_value())
-    dp = Dispatcher()
+    dp = Dispatcher(storage=RedisStorage(redis))
     dp.include_router(router)
     await dp.start_polling(bot)
     
