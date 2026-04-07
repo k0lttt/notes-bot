e9a44340aff553d8416d3ef8a1738e8348bde5e8
k0lttt <superpupkin07770@gmail.com>
2026-04-08 01:46:01 +0300

    commit 1.7.3 / BIGFIX

--- app/database/models.py
+++ app/database/models.py
@@ -1,7 +1,13 @@
 from sqlalchemy.ext.asyncio import (create_async_engine, async_sessionmaker,
                                     AsyncAttrs)
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import BigInteger, String, ForeignKey, func
+from sqlalchemy import (BigInteger, String, 
+                        ForeignKey, func, DateTime,
+                        Boolean)
+
+
+from datetime import datetime
+
 
 engine = create_async_engine(url="sqlite+aiosqlite:///database.db", 
                              echo=True)
@@ -19,5 +25,31 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
-    user_name: Mapped[str] = mapped_column(String(25), nullable=True)
-    timezone: Mapped[str] = mapped_column(String(31), nullable=False)
+    user_name: Mapped[str] = mapped_column(String(32), nullable=True)
+    timezone: Mapped[str] = mapped_column(String(31), default='UTC', nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, 
+        server_default=func.now()
+    )
+
+class Reminder(Base):
+    __tablename__ = 'reminders'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), 
+                                         nullable=False)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_sent: Mapped[bool] = mapped_column(Boolean,
+                                          default=False, 
+                                          nullable=False)
+    remind_at: Mapped[datetime] = mapped_column(DateTime, 
+                                                nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, 
+        server_default=func.now()
+    )
+
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
