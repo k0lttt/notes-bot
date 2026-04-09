388901b152ca3fbd36ef00de28bd5a99f1723ce9
k0lttt <superpupkin07770@gmail.com>
2026-04-09 14:19:15 +0300

    commit 1.7.10 / fix

--- app/handlers.py
+++ app/handlers.py
@@ -34,12 +34,12 @@ async def cmd_start(message: Message, state: FSMContext):
 
 @user.message(StateFilter('reg_name'))
 async def get_reg_name(message: Message, state: FSMContext):
-    await state.update_data(name=message.text.capitalize)
+    await state.update_data(name=message.text.capitalize())
     data = await state.get_data()
     await update_user(message.from_user.id, 
                       data['name'])
     await message.answer('Регистрация пройдена успешно!', 
-                        reply_markup = ReplyKeyboardRemove
+                        reply_markup = ReplyKeyboardRemove()
                         )
     await message.answer(
             f'🤝 Добро пожаловать, {message.from_user.full_name}! \n Я Notes-bot! Я буду напоминать тебе о важных для тебя событиях, стоит тебе только создать напоминание. Для начала работы нажми на "Создать Напоминание"', 
