02d2a85afa88c1f86a91e11a551ef3647919cd4b
k0lttt <superpupkin07770@gmail.com>
2026-04-23 22:26:56 +0300

    commit 1.8.2 fix2

--- app/database/requests.py
+++ app/database/requests.py
@@ -18,11 +18,9 @@ async def update_user(tg_id, name):
 
 async def timezone_check(tg_id):
     async with async_session() as session:
-        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        timezone = await session.scalar(select(User.timezone).where(User.tg_id == tg_id))
 
-    if user.timezone=="UTC":
-        return True
-    return False
+    return timezone=="UTC"
 
 async def get_title():
     pass
